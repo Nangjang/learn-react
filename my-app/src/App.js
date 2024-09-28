599eb992ead9758319717563d2b57d550e5b5fd3
Nangjang <jintrade23@gmail.com>
2024-09-28 17:49:05 +0900

    Ch. 4 State Management: Using 'useState'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [game, setGame] = useState('Mario');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My favourite game is {game}.</h1>
+      <button onClick={() => setGame('Zelda')}>Change Game</button>
+      <button onClick={() => setGame('Mario')}>Reset Game</button>
     </div>
   );
 }
