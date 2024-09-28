2de4898926978e52664322d5a93260cfcce16a6d
Nangjang <jintrade23@gmail.com>
2024-09-28 18:07:20 +0900

    Ch. 4 State Management: Understanding dependancy array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,16 +4,22 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [game, setGame] = useState('Mario');
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     console.log(`The game is ${game}.`);
-  }, [game]);
+  }, [game, count]);
+
+  useEffect(() => {
+    console.log(`The count is ${count}.`);
+  }, [count]);
 
   return (
     <div className="App">
       <h1>My favourite game is {game}.</h1>
       <button onClick={() => setGame('Zelda')}>Change Game</button>
       <button onClick={() => setGame('Mario')}>Reset Game</button>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
     </div>
   );
 }
